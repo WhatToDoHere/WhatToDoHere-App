8ab82d47f66f988c5244890375c765819f950b24
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-07-18 05:04:14 +0900

    Add: [T-04] 완료 목록 mock data 추가

---
+++ constants/todo.js
@@ -0,0 +1,20 @@
+export const COMPLETED_TODOS = [
+  {
+    id: '1',
+    title: '알고리즘 풀기',
+    location: '대한민국 서울특별시 성동구 독서당로 166',
+    friendName: null,
+  },
+  {
+    id: '2',
+    title: '회의록 작성하기',
+    location: '대한민국 서울특별시 강남구 테헤란로 522',
+    friendName: 'moon',
+  },
+  {
+    id: '3',
+    title: 'Workout',
+    location: '대한민국 서울특별시 강남구 테헤란로 522',
+    friendName: null,
+  },
+];
