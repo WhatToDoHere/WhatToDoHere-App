9d637384e76655026d6a6130a94c512b3cfc45f9
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-07-23 06:34:22 +0900

    Chore: [T-09] firestore, storage 세팅

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -13,5 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
 
-export default auth;
+export { auth, firestore, storage };
