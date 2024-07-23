098f2a76ffdf959fd079d663d6da346e9653a90b
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-07-24 04:15:51 +0900

    Feat: [T-09] 사용자별 location 정보 전역상태 추가
    - locationsAtom 추가

--- atoms.js
+++ atoms.js
@@ -4,3 +4,4 @@ export const userInfoAtom = atom(null);
 export const currentLocationAtom = atom(null);
 export const selectedLocationAtom = atom(null);
 export const wifiSSIDAtom = atom('');
+export const locationsAtom = atom([]);
