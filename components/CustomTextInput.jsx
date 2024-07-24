22eb762f0876503c491881a1261c5e1394cd6bcb
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-07-25 03:30:46 +0900

    Feat: [T-10] CustomTextInput 컴포넌트 추가

---
+++ components/CustomTextInput.jsx
@@ -0,0 +1,68 @@
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from 'react-native';
+
+export function CustomTextInput({
+  value,
+  onChangeText,
+  placeholder,
+  multiline = false,
+}) {
+  return (
+    <View style={[styles.container, multiline && styles.multilineContainer]}>
+      <TextInput
+        style={[styles.input, multiline && styles.multilineInput]}
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        multiline={multiline}
+      />
+      {value.length > 0 && (
+        <TouchableOpacity
+          onPress={() => onChangeText('')}
+          style={styles.clearButton}
+        >
+          <Image
+            source={require('../assets/icons/icon-clear.png')}
+            style={styles.clearIcon}
+          />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#EEEEEE',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  multilineContainer: {
+    alignItems: 'flex-start',
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    fontFamily: 'Pretendard-Regular',
+    fontSize: 15,
+    color: '#202020',
+  },
+  multilineInput: {
+    height: 100,
+    textAlignVertical: 'top',
+  },
+  clearButton: {
+    padding: 10,
+  },
+  clearIcon: {
+    width: 15,
+    height: 15,
+  },
+});
