dd9b1719fddda03d4b3c6f0efe7dea307a4a737b
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-07-26 17:59:19 +0900

    Feat: [T-11] DeleteButton 공통 컴포넌트 추가 (Location, Todo 삭제)

---
+++ components/DeleteButton.jsx
@@ -0,0 +1,38 @@
+import { TouchableOpacity, Image, StyleSheet } from 'react-native';
+
+export default function DeleteButton({ onPress }) {
+  return (
+    <TouchableOpacity style={styles.deleteButton} onPress={onPress}>
+      <Image
+        source={require('../assets/icons/icon-delete.png')}
+        style={styles.deleteIcon}
+      />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  deleteButton: {
+    position: 'absolute',
+    left: '50%',
+    bottom: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 50,
+    height: 50,
+    marginLeft: -25,
+    padding: 10,
+    borderRadius: 50,
+    backgroundColor: '#F15858',
+    shadowColor: '#707070',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  deleteIcon: {
+    width: 22,
+    height: 22,
+  },
+});
