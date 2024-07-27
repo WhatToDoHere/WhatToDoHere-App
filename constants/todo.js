322a7e78ac5740928021b357343a456c0bfb5d01
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-07-28 07:39:12 +0900

    Add: [T-12] todo 기본 데이터 구조 상수화

--- constants/todo.js
+++ constants/todo.js
@@ -4,3 +4,15 @@ export const REMINDER_TRIGGER = {
   ARRIVE: '도착할 때',
   LEAVE: '떠날 때',
 };
+export const DEFAULT_TODO_DATA = {
+  title: '',
+  memo: '',
+  image: null,
+  reminder: {
+    isEnabled: false,
+    reminderOnArrival: true,
+    delayMinutes: 0,
+  },
+  locationId: '',
+  assignedBy: '',
+};
