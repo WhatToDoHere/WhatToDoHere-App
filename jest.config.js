a90220ea181b8f29fd0b44ca984fd01846ff9446
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-08-19 17:06:41 +0900

    Test: Unit Test Setting
    - jest.config.js 세팅
    - @testing-library/react-native 설치
    - @testing-library/jest-native 설치
    - react-test-renderer 설치

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.expo/'],
+  moduleDirectories: ['node_modules'],
+};
