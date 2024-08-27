0f2c1139b38073707ed0fc675c32f20be4b81609
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-08-28 03:30:17 +0900

    Chore: Unit Test Setting

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,20 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.expo/'],
   moduleDirectories: ['node_modules'],
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/components/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/__tests__/**',
+    '!**/*.test.{js,jsx,ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
 };
